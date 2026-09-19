dc65ac860d54a510be05d70eaedbef5d00f3c4da
Thabang Mokgonyana <thabangmokgonyana@gmail.com>
2026-09-19 18:17:57 +0200

    Created support file for the Stock take ref generator

---
+++ app/Support/StockTakeReferenceGenerator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Support;
+
+use App\Models\StockTake;
+use Illuminate\Support\Str;
+
+final class StockTakeReferenceGenerator
+{
+    /**
+     * Format: STK-YYYYMMDD-XXXX
+     */
+    public static function make(): string
+    {
+        do {
+            $reference = 'STK-'.now()->format('Ymd').'-'.Str::upper(Str::random(4));
+        } while (StockTake::where('reference', $reference)->exists());
+
+        return $reference;
+    }
+}
